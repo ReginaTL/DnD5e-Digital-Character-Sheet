58a6507d435e5d928f4eac3a7c951665b47d3213
Regina Locicero <rtl3971@rit.edu>
2017-05-13 22:51:46 -0400

    Database initialized finished

--- DnD5e-Digital-Character-Sheet/www/js/app.js
+++ DnD5e-Digital-Character-Sheet/www/js/app.js
@@ -4,9 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $cordovaSQLite, $rootScope) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -19,6 +19,28 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    // Open the Database
+    if (window.cordova) {
+        // App/device syntax
+        $rootScope.db = $cordovaSQLite.openDB({ name: "database.db" });
+    } else {
+        // Ionic serve syntax
+        $rootScope.db = window.openDatabase("database.db", '1.0', 'App Demo', 65536);
+    }
+
+    // Setup Tables
+    $cordovaSQLite.execute($rootScope.db, "CREATE TABLE character(id INT PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), race VARCHAR(255), class VARCHAR(255), level INT, experience INT, alignment VARCHAR(255), background VARCHAR(255), hp INT, speed INT, proficiency INT, hitdice VARCHAR(55), ac INT, strength INT, intelligence INT, constitution INT, dexterity INT, charisma INT, wisdom INT, spellAbility VARCHAR(255), spellSaveDC INT, spellAttackBonus INT, pp INT, gp INT, ep INT, sp INT, cp INT);");
+    $cordovaSQLite.execute($rootScope.db, "CREATE TABLE skills(name VARCHAR(255) PRIMARY KEY, baseType VARCHAR(3));");
+    $cordovaSQLite.execute($rootScope.db, "CREATE TABLE equipment(id INT PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), damageType VARCHAR(255), damageDice, VARCHAR(255), bonus INT);");
+    $cordovaSQLite.execute($rootScope.db, "CREATE TABLE char_equip(charID INT, equipID INT);");
+    $cordovaSQLite.execute($rootScope.db, "CREATE TABLE spells(id INT PRIMARY KEY AUTOINCREMENT, level INT);");
+    $cordovaSQLite.execute($rootScope.db, "CREATE TABLE char_spells(charID INT, spellID INT);");
+    $cordovaSQLite.execute($rootScope.db, "CREATE TABLE languages(id INT PRIMARY KEY AUTOINCREMENT, name VARCHAR(255));");
+    $cordovaSQLite.execute($rootScope.db, "CREATE TABLE char_lang(charID INT, langID INT);");
+    // Setup Skils - always the same
+    $cordovaSQLite.execute($rootScope.db, "INSERT INTO skills(name, basType) VALUES('Acrobatics', 'DEX'),('Animal Handling', 'WIS'), ('Arcana', 'INT'), ('Athletics', 'STR'), ('Deception', 'CHA'), ('History', 'INT'), ('Insight', 'WIS'), ('Intimidation', 'CHA'), ('Invesitgation', 'INT'), ('Medicine', 'WIS'), ('Nature', 'INT'), ('Perception', 'WIS'), ('Performance', 'CHA'), ('Persuasion', 'CHA'), ('Religion', 'CHA'), ('Sleight of Hand', 'DEX'), ('Stealth', 'DEX'), ('Survival', 'WIS');");
+    // Setup example data
+    $cordovaSQLite.execute($rootScope.db, "INSERT INTO character(name, race, class, level, experience, alignment, background, hp, speed, proficiency, hitdice, ac, strength, intelligence, constitution, dexterity, charisma, wisdom, spellAbility, spellSaveDC, spellAttackBonus, pp, gp, ep, sp, cp) VALUES ('Madame Daphne Blanchet Benoist', 'Tiefling', 'Warlock', 5, 0, 'neutral', 'courtier', 27, 30, 3, '5d8', 14, 12, 16, 15, 15, 19, 18, 'CHA', 15, 6, 45, 220, 0, 0, 0);");
   });
 })
 
